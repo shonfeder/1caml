73fae72b2d51c8b47873e440175c69fa3a7c2c1d
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-11-14 12:13:46 -0700

    Create CLI

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,1 +1,28 @@
-let () = print_endline OneCaml.message
+open Cmdliner
+
+let cmd_help: unit Lwt.t Term.t * Term.info =
+  let doc = "show help" in
+  Term.
+  ( ret @@ pure @@ `Help ( `Pager, None )
+  , info "help" ~doc
+  )
+
+let cmd_default =
+  Term.
+  ( ret @@ pure @@ `Help ( `Pager, None )
+  , info "1caml" ~version:"0.0.0"
+  )
+
+let cmds = [
+  cmd_help;
+]
+
+let main () =
+  match Term.eval_choice cmd_default cmds with
+  | `Error e -> exit 1
+  | `Ok expr -> Lwt_main.run expr
+  | _ -> exit 0
+
+let () =
+  if not !Sys.interactive then
+    main ()
