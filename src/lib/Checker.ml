625dbaf5a48b5329a67f7a3dc55c9fb8375c537e
Darin Morrison <freebroccolo@users.noreply.github.com>
2018-02-28 17:07:08 -0700

    Move tests into separate directory

--- src/lib/Checker.ml
+++ src/lib/Checker.ml
@@ -33,67 +33,48 @@ module Stack = struct
   [@@deriving (eq, ord, show)]
 end
 
+module State = struct
+  type t = Continuation.t * Stack.t
+  [@@deriving (eq, ord, show)]
+end
+
 let check (gamma : Context.t) =
-  let rec go (ms : Instruction.t list) (ts : Program.t) (fs : Continuation.t) (xs : Stack.t) : (Continuation.t * Stack.t) option =
+  let rec go (ms : Instruction.t list) (ts : Program.t) (fs : Continuation.t) (xs : Stack.t) : State.t option =
     let open Instruction in
     let open Token in
     let open Type in
     match gamma, ms, ts, fs, xs with
     (* halt *)
-    | _gamma, _ms, [], fs, xs -> Some (fs, xs)
+    | _gamma, _ms, [], fs, xs ->
+      Some (fs, xs)
     (* id-hd *)
     | gamma, (Head :: ms), (IDENTIFIER t :: ts), fs, xs ->
       begin
         match Caml.List.assoc_opt t gamma with
-        | Some tau -> go (Spine :: ms) ts (tau :: fs) xs
-        | _ -> None
+        | Some tau ->
+          go (Spine :: ms) ts (tau :: fs) xs
+        | _ ->
+          None
       end
     (* id-sp-[] *)
     | gamma, (Spine :: ms), (IDENTIFIER t :: ts), (Fun (sigma , tau) :: fs), [] ->
       begin
         match Caml.List.assoc_opt t gamma with
-        | Some sigma' when Type.equal sigma' sigma -> go (Spine :: ms) ts (tau :: fs) []
-        | _ -> None
+        | Some sigma' when Type.equal sigma' sigma ->
+          go (Spine :: ms) ts (tau :: fs) []
+        | _ ->
+          None
       end
     (* id-sp-:: *)
-    | _gamma, (Spine :: ms), ts, (Fun (sigma , tau) :: fs), (sigma' :: xs) when Type.equal sigma' sigma -> go (Spine :: ms) ts (tau :: fs) xs
+    | _gamma, (Spine :: ms), ts, (Fun (sigma , tau) :: fs), (sigma' :: xs) when Type.equal sigma' sigma ->
+      go (Spine :: ms) ts (tau :: fs) xs
     (* lp *)
-    | _gamma, ms, (LEFT_PARENTHESIS :: ts), fs, xs -> go (Head :: ms) ts fs xs
+    | _gamma, ms, (LEFT_PARENTHESIS :: ts), fs, xs ->
+      go (Head :: ms) ts fs xs
     (* rp *)
-    | _gamma, (_m :: ms), (RIGHT_PARENTHESIS :: ts), (tau :: fs), [] -> go ms ts fs (tau :: [])
-    | _ -> None
+    | _gamma, (_m :: ms), (RIGHT_PARENTHESIS :: ts), (tau :: fs), [] ->
+      go ms ts fs (tau :: [])
+    | _ ->
+      None
   in
   go
-
-module Examples = struct
-  open Instruction
-  open Token
-  open Type
-
-  let context = [
-    ("zero", Nat);
-    ("succ", Fun(Nat, Nat));
-    ("add", Fun(Nat, Fun(Nat, Nat)));
-    ("map", Fun(Fun(Nat, Nat), Fun(Seq, Seq)));
-  ]
-
-  let test p = check context (Head :: []) p [] []
-
-  (* Some ([], [Nat]) *)
-  let p0 = LEFT_PARENTHESIS :: IDENTIFIER "succ" :: IDENTIFIER "zero" :: RIGHT_PARENTHESIS :: []
-
-  (* Some ([], [Nat]) *)
-  let p1 = LEFT_PARENTHESIS :: IDENTIFIER "succ" :: LEFT_PARENTHESIS :: IDENTIFIER "succ" :: IDENTIFIER "zero" :: RIGHT_PARENTHESIS :: RIGHT_PARENTHESIS :: []
-
-  (* Some ([], [Nat]) *)
-  let p2 = LEFT_PARENTHESIS :: IDENTIFIER "add" :: LEFT_PARENTHESIS :: IDENTIFIER "succ" :: IDENTIFIER "zero" :: RIGHT_PARENTHESIS :: IDENTIFIER "zero" :: RIGHT_PARENTHESIS :: []
-
-  (* Some ([], [Fun (Nat, Nat)]) *)
-  let p3 = LEFT_PARENTHESIS :: IDENTIFIER "succ" :: RIGHT_PARENTHESIS :: []
-
-  (* Some ([Fun (Nat, Nat)]. []) *)
-  let p4 = LEFT_PARENTHESIS :: IDENTIFIER "add" :: IDENTIFIER "zero" :: []
-
-  (* Some ([], [Fun (Seq, Seq)]) *)
-  let p5 = LEFT_PARENTHESIS :: IDENTIFIER "map" :: LEFT_PARENTHESIS :: IDENTIFIER "add" :: IDENTIFIER "zero" :: RIGHT_PARENTHESIS :: RIGHT_PARENTHESIS :: []
-end
