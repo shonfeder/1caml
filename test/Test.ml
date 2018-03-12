4f1997d355536257775c35914cc351de9a5a0e47
Darin Morrison <freebroccolo@users.noreply.github.com>
2018-03-11 22:04:40 -0600

    Refactor tests

--- test/Test.ml
+++ test/Test.ml
@@ -1,8 +1,27 @@
 open OneCaml
 open SystemF
 
-let () =
-  let ctx = Context.Stop in
-  let trm = Term.Unit in
-  let res = Term.infer ctx trm in
-  Fmt.option Term.Infer.pp Fmt.stdout res
+module Infer = struct
+  let pp_trm fmt trm =
+    let ctx = Context.Stop in
+    let res = Term.infer ctx trm in
+    Fmt.option Term.Infer.pp fmt res
+
+  let test000 =
+    Term.Unit
+
+  let test001 =
+    Term.Ann { trm = Term.Unit; typ = Type.Unit }
+
+  let () =
+    let f i k =
+      Fmt.pf Fmt.stdout "@[%a%a@,@[<v>%a@]@]@\n@."
+        Fmt.string "test #"
+        Fmt.int i
+        pp_trm k in
+    let tests = [
+      test000;
+      test001;
+    ] in List.iteri tests ~f
+end
+
